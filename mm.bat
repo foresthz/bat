56a751af202a14fd02eaf0aa5a4ddf82fbab9a0a
home <home@tom.com>
2014-11-29 04:09:58 +0800

    mm

--- mm.bat
+++ mm.bat
@@ -1,13 +1,19 @@
 git add . -A
 if {%1}=={} goto setVal
 rem "pull push, using [pp | branch ] as the first parameter, no need to use quotation marks"
-if {%1}=={pp} goto pull
+rem "nopush | pull | push"
+rem "nps 不提交到远程"
+rem "pp 先从远程获取更新"
+if {%1}=={pull} goto pull
+if {%1}=={push} goto push
+
 set comment="%*"
 goto commit
 
 :setVal
 set comment="mm"
 goto commit
+
 :pull
 set comment="pull"
 git pull origin %2
@@ -15,4 +21,9 @@ goto commit
 
 :commit
 git commit -m %comment%
+if {%1}=={nopush} goto finish
+
+:push
 git push origin
+
+:finish
