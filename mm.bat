4c5cf365dc52ff475c85060f5bc0fc8f3f6439dc
forest <forest@tom.com>
2015-01-20 06:15:53 +0800

    pull

--- mm.bat
+++ mm.bat
@@ -1,18 +1,22 @@
 git add . -A
+
 if {%1}=={} goto setVal
 rem "pull push, using [pp | branch ] as the first parameter, no need to use quotation marks"
 if {%1}=={pp} goto pull
 set comment="%*"
-goto commit
+git commit -m %comment%
+goto push
 
 :setVal
 set comment="mm"
-goto commit
+git commit -m %comment%
+goto push
+
 :pull
 set comment="pull"
-git pull origin %2
-goto commit
-
-:commit
 git commit -m %comment%
+git pull origin %2
+goto push
+
+:push
 git push origin
